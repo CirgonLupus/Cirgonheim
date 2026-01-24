7babd3d960ae14fe95b51a22085844b0f4c4448f
CirgonLupus <medusa.pih@gmail.com>
2026-01-24 20:48:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+const gateSound = new Audio("sounds/open_gate.mp3");
+
 function enterCity() {
+    gateSound.play();
+
     document.querySelector('.left').style.transform = "translateX(-100%)";
     document.querySelector('.right').style.transform = "translateX(100%)";
+
+    // Efekt oślepienia po otwarciu bramy
+    setTimeout(() => {
+        document.querySelector('.flash').style.opacity = "1";
+    }, 1500);
+
+    // Przejście do kolejnej dzielnicy
+    setTimeout(() => {
+        window.location.href = "world/district1.html";
+    }, 2500);
 }
