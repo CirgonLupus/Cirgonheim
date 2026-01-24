44fa00e35a190fab418c52f6348872ce6ee583dc
CirgonLupus <medusa.pih@gmail.com>
2026-01-24 19:31:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function enterCity() {
-    document.querySelector('gate_left.jpg').style.transform = "translateX(-100%)";
-    document.querySelector('gate_right.jpg').style.transform = "translateX(100%)";
+    document.querySelector('img/gate_left.jpg').style.transform = "translateX(-100%)";
+    document.querySelector('img/gate_right.jpg').style.transform = "translateX(100%)";
 }
