34d2fb15b8de26f6e9d421e3192b7160b3b6887a
CirgonLupus <medusa.pih@gmail.com>
2026-02-01 08:24:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const btn = document.getElementById('enter-btn');
-const audio = new Audio('sound/open.mp3'); // ścieżka do dźwięku otwierania
+const audio = new Audio('sound/open.mp3'); // ścieżka do dźwięku
 
 btn.addEventListener('click', () => {
     audio.play(); // dźwięk otwierania
 
     document.body.classList.add('open'); // animacja wrót
 
-    // rozjaśnienie po chwili
+    // rozjaśnienie
     setTimeout(() => {
         document.querySelector('.flash').classList.add('active');
     }, 1500);
 
-    // przejście do kolejnej dzielnicy
+    // przejście do kolejnej strony
     setTimeout(() => {
         window.location.href = "dzielnica.html";
     }, 2600);
