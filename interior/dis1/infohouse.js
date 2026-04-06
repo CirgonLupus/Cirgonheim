49519f26c8d7fe5682abdfb11e637b181c74c945
CirgonLupus <medusa.pih@gmail.com>
2026-04-06 18:07:49 +0200

    Update infohouse.js

--- interior/dis1/infohouse.js
+++ interior/dis1/infohouse.js
@@ -85,7 +85,7 @@ function openBook(id, lang) {
     const text = document.getElementById('cornua-text');
 
     // OGÓLNE ustawienie umbilicusa x2
-    setUmbilicus({ scale: 2, x: 0, y: -300 });
+    setUmbilicus({ scale: 4, x: 0, y: -300 });
 
     // NADPISYWANIE USUNIĘTE
 
