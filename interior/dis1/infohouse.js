fd2862c83d034023f74ac81cc25f4daf938c9b6a
CirgonLupus <medusa.pih@gmail.com>
2026-04-08 12:05:36 +0200

    Update infohouse.js

--- interior/dis1/infohouse.js
+++ interior/dis1/infohouse.js
@@ -145,7 +145,7 @@ function openBook(id, lang) {
     void cornua.offsetWidth;
 
     // cornua wjeżdża z góry na docelową pozycję
-    cornua.style.top = '0%';
+    cornua.style.top = '30%';
 
     setTimeout(() => {
         text.style.opacity = '1';
