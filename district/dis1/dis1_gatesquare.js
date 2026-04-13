4f0967a82c626ca4fa074b2ea64b96f86455a089
CirgonLupus <medusa.pih@gmail.com>
2026-04-13 13:38:46 +0200

    Update dis1_gatesquare.js

--- district/dis1/dis1_gatesquare.js
+++ district/dis1/dis1_gatesquare.js
@@ -115,3 +115,9 @@ window.addEventListener('pageshow', (e) => {
         fadeOutOnEnter();
     }
 });
+document.addEventListener('mousemove', e => {
+    const x = (e.clientX / window.innerWidth - 0.5) * 4; 
+    const y = (e.clientY / window.innerHeight - 0.5) * 4;
+
+    document.body.style.backgroundPosition = `${50 + x}% ${50 + y}%`;
+});
